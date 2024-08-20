d1f1a7ac6d9bf7c7826a698253ddc153716c5620
Josh Gochey <josh@gochey.net>
2024-08-19 23:16:19 -0500

    Fixed  to actually work.

--- public/main.js
+++ public/main.js
@@ -12,29 +12,40 @@ const init = () => {
     <div id="text">Click the joke button!</div>
     <div id="delivery"> </div>
   `;
+};
 
-  document.querySelector('#click-me').addEventListener('click', () => {
-    getRequest().then((response) => {
-      console.warn(response.setup);
-      console.warn(response.delivery);
-      document.querySelector('#text').innerHTML = response.setup;
-      document.querySelector('#app').innerHTML = `<h1>Joke Generator</h1>
+const joker = () => {
+  // Restart from here after get another joke
+
+  getRequest().then((response) => {
+    console.warn(response.setup);
+    console.warn(response.delivery);
+    // document.querySelector('#text').innerHTML = response.setup;
+    document.querySelector('#app').innerHTML = `<h1>Joke Generator</h1>
     <button class="btn btn-danger" id="click-me-too">Get punchline</button><br />
     <hr />
     <div id="text">${response.setup}</div>
     <div id="delivery"> </div>`;
 
-      document.querySelector('#click-me-too').addEventListener('click', () => {
-        document.querySelector('#delivery').innerHTML = response.delivery;
-        document.querySelector('#click-me-too').innerHTML = 'Get another joke';
+    document.querySelector('#click-me-too').addEventListener('click', () => {
+      document.querySelector('#app').innerHTML = `<h1>Joke Generator</h1>
+    <button class="btn btn-danger" id="click-me-three">Get another joke</button><br />
+    <hr />
+    <div id="text">${response.setup}</div>
+    <div id="delivery">${response.delivery} </div>`;
+
+      document.querySelector('#click-me-three').addEventListener('click', () => {
+        joker();
       });
     });
   });
-
-  // Everything works...'cept how do the Double-click thing without restarting from the beginning?
-
-  // USE WITH FIREBASE AUTH
-  // ViewDirectorBasedOnUserAuthStatus();
 };
 
 init();
+
+document.querySelector('#click-me').addEventListener('click', () => {
+  joker();
+});
+
+// USE WITH FIREBASE AUTH
+// ViewDirectorBasedOnUserAuthStatus();
